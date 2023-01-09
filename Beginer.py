80060af64b4e15cec870089aae69f64b236f14e5
Bobirdeveloper <84250735+Bobirdeveloper@users.noreply.github.com>
2023-01-09 17:54:12 +0500

    hammasi yaxshi

--- Beginer.py
+++ Beginer.py
@@ -1,10 +1,11 @@
 # lesson (list) 9.1.2023
-ismlar = ["abror", 'axror',"abdulaziz"]
+ismlar = ["abror", 'axror', "abdulaziz"]
 print(f"Salom {ismlar[0].title()} qandaysan!!!")
 print(f"Nima gaplar {ismlar[1].title()} tinchmi?")
 print(f"Qayerlarda yuribsan {ismlar[2].title()}?")
-numbers = [2,-3, 5.5, -2.1,3465,7356,2,2]
+numbers = [2, -3, 5.5, -2.1, 3465, 7356, 2, 2]
 # number = numbers.pop(2)
 # numbers.insert(4,'hammasi yaxshi')
-del numbers[2]
-print(numbers)
+# del numbers[2]
+# print(numbers)
+# hammasi yaxshi
