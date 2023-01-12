0e39f86b761316db4a362b7bc45f474589fc40bc
Bobirdeveloper <84250735+Bobirdeveloper@users.noreply.github.com>
2023-01-12 15:15:06 +0500

    hammasi yaxshi

---
+++ dict.py
@@ -0,0 +1,5 @@
+# 14-dars: Dictionary
+oila = {'otam': {'ismi': 'Uroqov Baxodir', 't_yili': 1967, 't_joy': 'Navoiy viloyati Qiziltepa tumani'},
+        'onam': {'ismi': 'Shodmonova Sharifa', 't_yili': 1967, 't_joy': "Navoiy viloyati Qiziltepa tumani"}}
+print(f"Otam {oila['otam']['ismi']} {oila['otam']['t_yili']}-yili {oila['otam']['t_joy']}da tavallud topgan!")
+print(f"Onam {oila['onam']['ismi']} {oila['onam']['t_yili']}-yili {oila['onam']['t_joy']}da tavallud topgan!")
